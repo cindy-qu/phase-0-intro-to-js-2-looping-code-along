e62e79916abd25700d191d0c55edbfdb7bf56503
Cindy Qu <cindyqu42@gmail.com>
2022-08-01 03:33:13 -0700

    commit tree 0e6ee0e6a4795ed487cbfa6b13d9013785051de7

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solutions in this file
+
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+
+//     }
+//     return gifts;
+// }
+// wrapGifts(gifts);
+
+
+
+function writeCards(name, event) {
+    let message = []
+    for (let i = 0; i < name.length; i++) {
+
+        message.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+
+    }
+
+    return message;
+}
+
+function countDown(amount) {
+    let number = amount;
+    while (number >= 0) {
+        console.log(number--)
+    }
+}
